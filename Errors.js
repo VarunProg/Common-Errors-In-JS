ce23361d5b50f091d89d4ff83f744812010ff9ae
VarunProg <89865506+VarunProg@users.noreply.github.com>
2022-01-14 13:43:08 +0100

    trying to access value outside of the scope

--- Errors.js
+++ Errors.js
@@ -1,2 +1,10 @@
 // reference error
+// console.log(a);
+//trying to access value outside of the scope
+
+function demo() {
+    let a = 10;
+}
 console.log(a);
+let output = demo();
+console.log(output);
